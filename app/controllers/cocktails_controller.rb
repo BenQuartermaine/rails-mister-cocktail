588d1a8cd3ed51bdf986e40057b1ab4572adbe6f
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-11-16 17:28:32 +0800

    changed root to index, deleted extra .save line in cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,8 +13,6 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-
     if @cocktail.save
       redirect_to new_cocktail_dose_path(@cocktail)
     else
